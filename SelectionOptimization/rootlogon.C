e0384e8ec15e611c9ac96601202653ef8ca6f5fd
lovedeepkaursaini <lovedeep@cern.ch>
2015-01-12 09:31:56 -0600

    rm

--- SelectionOptimization/rootlogon.C
+++
@@ -1,9 +0,0 @@
-{
-
-  gROOT->ProcessLine(".L Variables.hh+");
-  gROOT->ProcessLine(".L VariableLimits.hh+");
-  gROOT->ProcessLine(".L VarCut.cc+");
-  gROOT->ProcessLine(".L optimize.cc+");
-
-}
-
