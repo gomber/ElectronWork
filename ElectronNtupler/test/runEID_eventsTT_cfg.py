840a4025cd462c4d4b4fac321d0e8665af12cbad
lovedeepkaursaini <lovedeep@cern.ch>
2015-01-13 08:42:51 -0600

    events

--- ElectronNtupler/test/runEID_eventsTT_cfg.py
+++ ElectronNtupler/test/runEID_eventsTT_cfg.py
@@ -8,8 +8,8 @@ process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(
 '/store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/00C90EFC-3074-E411-A845-002590DB9262.root',
 )
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.MessageLogger.cerr.FwkReport.reportEvery=cms.untracked.int32(1000)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.MessageLogger.cerr.FwkReport.reportEvery=cms.untracked.int32(10)
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 
 process.ntupler = cms.EDAnalyzer('ElectronNtuplerEventStructure',
